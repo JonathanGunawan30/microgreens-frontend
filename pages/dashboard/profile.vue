6ed13e618e535e4d38bccfe826dc65e88a541be1
Jonathan Gunawan <jgunawan3005@gmail.com>
2026-04-13 15:57:14 +0700

    fix(map): replace OSM tile layer with CartoDB Voyager to fix 403 blocked error

--- pages/dashboard/profile.vue
+++ pages/dashboard/profile.vue
@@ -404,9 +404,10 @@ const openMapModal = () => {
 
     mapInstance.value = window.L.map('admin-location-map').setView([tempLocation.value.lat, tempLocation.value.lng], 16)
 
-    window.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors',
-      maxZoom: 19
+    window.L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(mapInstance.value)
 
     mapMarker.value = window.L.marker([tempLocation.value.lat, tempLocation.value.lng], {
